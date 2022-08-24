4ca780e9dfaba5081f3d83c97a31fbe65db45990
iulian-lupastean <101259673+iulian-lupastean@users.noreply.github.com>
2022-08-24 13:34:11 +0300

    Update UpdateDelete.sql

--- UpdateDelete.sql
+++ UpdateDelete.sql
@@ -6,11 +6,13 @@ Where ID=12
 Delete From ProjectEmployees
 Where ID=3
 
+-- Un coleg s-a mutat din republica moldova in romania
 Update Employees
 Set Country='Romania',
 	City='Timisoara'
 Where Id=11
 
+-- A crescut minimul pe care il luam pe un proiect
 Update Projects
 Set Revenue = 100000
 Where Revenue < 100000
