31a5de5647d05bc1bd979ba64b67fc338f1da2a2
iulian-lupastean <101259673+iulian-lupastean@users.noreply.github.com>
2022-08-24 13:26:46 +0300

    Update UpdateDelete.sql

--- UpdateDelete.sql
+++ UpdateDelete.sql
@@ -6,4 +6,12 @@ Where ID=12
 Delete From ProjectEmployees
 Where ID=3
 
+Update Employees
+Set Country='Romania',
+	City='Timisoara'
+Where Id=11
+
+Update Projects
+Set Revenue = 100000
+Where Revenue < 100000
 
