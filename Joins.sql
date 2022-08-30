ae546f8cbb47a6c47ce14c592e9fee7646978562
iulian-lupastean <101259673+iulian-lupastean@users.noreply.github.com>
2022-08-30 18:53:35 +0300

    Update Joins.sql

--- Joins.sql
+++ Joins.sql
@@ -5,3 +5,11 @@ FROM Employees AS E
 INNER JOIN PhoneNumbers as PN
 			ON E.ID=PN.PersonID
 GROUP BY LastName,FirstName,PhoneNumber
+
+
+--Left and right join for many to many
+SELECT e.FirstName, e.LastName, e.Country, p.ProjectName
+FROM dbo.Employees as e
+	LEFT JOIN dbo.ProjectEmployees as pe ON e.ID = pe.EmployeeId
+	RIGHT JOIN dbo.Projects as p ON p.ID = pe.ProjectId
+ORDER BY p.ProjectName
