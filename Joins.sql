9d5d9a55525f39aceb8114bec3adab8258b24f4b
iulian-lupastean <101259673+iulian-lupastean@users.noreply.github.com>
2022-08-30 15:12:03 +0300

    Update Joins.sql

--- Joins.sql
+++ Joins.sql
@@ -1,1 +1,7 @@
+-- INNER JOIN FOR 1 TO MANY
 
+SELECT E.FirstName,E.LastName,PN.PhoneNumber
+FROM Employees AS E
+INNER JOIN PhoneNumbers as PN
+			ON E.ID=PN.PersonID
+GROUP BY LastName,FirstName,PhoneNumber
