f8dc9029e6c29d72b1ed8e1d7c26a3f79c83ace3
iulian-lupastean <101259673+iulian-lupastean@users.noreply.github.com>
2022-10-17 14:31:38 +0300

    Update Seeder.sql

--- Seeder.sql
+++ Seeder.sql
@@ -1,4 +1,4 @@
-INSERT INTO dbo.Employees (InternalIdentifier, FirstName, LastName, City, Country, BirthYear, NOTES)
+INSERT INTO Employees (InternalIdentifier, FirstName, LastName, City, Country, BirthYear, NOTES)
 VALUES
 ('000001', 'Dan', 'Patrascu-Baba', 'Timisoara', 'Romania', 1984, NULL),
 ('000002', 'Adina', 'Moldovan', 'Timisoara', 'Romania', 1988, NULL),
@@ -6,6 +6,7 @@ VALUES
 ('000004', 'Vesela', 'Topalova', 'Sofia', 'Bulgaria', 1980, NULL),
 ('000005', 'Constantin', 'Bivol', 'Chisinau', 'Moldova', 2000, NULL),
 ('000006', 'Vlad', 'Mihet', 'Timisoara', 'Romania', 2002, NULL),
+('000007', 'Lupastean', 'Iulian', 'Cluj-Napoca', 'Romania', 2010, NULL),
 ('000008', 'Loredana', 'Filip', 'Timisoara', 'Romania', 1992, NULL),
 ('000009', 'Sabina', 'Paulesco', 'Chisinau', 'Moldova', 1994, NULL),
 ('000010', 'Anna', 'Grier', 'Timisoara', 'Romania', 1990, NULL),
@@ -17,14 +18,15 @@ VALUES
 
 
 
-INSERT INTO dbo.Projects (ProjectName, ProjectDescription, CustomerName, Revenue)
+
+INSERT INTO Projects (ProjectName, ProjectDescription, CustomerName, Revenue)
 VALUES
 ('ProjectPlan', NULL, 'Amdaris', 0),
 ('Project Tango', NULL, 'Microsoft', 1150489/78),
 ('Hello world', NULL, 'Oracle', 159753.45),
 ('Meta', NULL, 'Meta Corporation', 78456.65)
 
-INSERT INTO dbo.ProjectEmployees (ProjectId, EmployeeId)
+INSERT INTO ProjectEmployees (ProjectId, EmployeeId)
 VALUES
 (1, 8),
 (1, 5),
